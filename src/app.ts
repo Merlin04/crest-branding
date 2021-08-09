61fa836ddbedfc41422ef2f5a6ecc4dbd79c92e1
merlin04 <Merlin04@users.noreply.github.com>
2021-08-09 13:36:00 -0700

    Add request handler

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
 import express from 'express';
+import { inline, stacked } from './brand';
 
 const app = express();
 const port = 3000;
 
-app.get('/stacked.png', (req, res) => {
+const dollarEscape = (l: string[]) => l.map(arg => decodeURIComponent(arg.replaceAll("$", "%")));
+
+const responseHandler = (isInline: boolean) => async (req: express.Request, res: express.Response) => {
     const agency = (!Array.isArray(req.query.agency)) ? [req.query.agency] : req.query.agency;
     // TODO: limit height so we don't crash the server
     const height = Number(req.query.height ?? "800");
     const square = Boolean(req.query.square);
-    res.send('Hello World!');
-});
+
+    const image = await (isInline ? inline : stacked)(dollarEscape(agency.map(item => typeof item === "string" ? item : "")), height, square);
+
+    res.type("png");
+    res.send(image);
+};
+
+app.get("/stacked.png", responseHandler(false));
+app.get("/inline.png", responseHandler(true));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
