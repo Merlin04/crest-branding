004b35c04c33bd91b37b907a82a9f23e29ae9917
merlin04 <Merlin04@users.noreply.github.com>
2021-08-09 20:57:36 -0700

    Use VercelRequest/Response

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { inline, stacked } from './brand';
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const dollarEscape = (l: string[]) => l.map(arg => decodeURIComponent(arg.replaceAll("$", "%")));
 
-export default (isInline: boolean) => async (req: express.Request, res: express.Response) => {
+export default (isInline: boolean) => async (req: express.Request | VercelRequest, res: express.Response | VercelResponse) => {
     const agency = (!Array.isArray(req.query.agency)) ? [req.query.agency] : req.query.agency;
     // TODO: limit height so we don't crash the server
     const height = Number(req.query.height ?? "800");
@@ -12,6 +13,6 @@ export default (isInline: boolean) => async (req: express.Request, res: express.
 
     const image = await (isInline ? inline : stacked)(dollarEscape(agency.map(item => typeof item === "string" ? item : "")), height, square, govt);
 
-    res.type("png");
+    res.setHeader("Content-Type", "image/png");
     res.send(image);
 };
